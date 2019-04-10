9f3dac3bff066b4ea25d5ea9b450cb1364c9e13e
Kaai Suzuki <suzydp@me.com>
2019-04-09 23:35:16 -0700

    Apply footer

--- index.php
+++ index.php
@@ -48,6 +48,9 @@
       }
     ?>
   </main>
+  <footer class="footer">
+    <p>&copy;<?php echo date('Y')?> Z magazine</p>
+  </footer>
   <script type="text/javascript" src="<?= get_template_directory_uri(); ?>/assets/js/main.js"></script>
 </body>
 </html>
