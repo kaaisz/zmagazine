6d34f36edf4154798899973749a4c9efc9657dc0
Kaai Suzuki <suzydp@me.com>
2019-04-11 11:59:34 -0700

    Disable gallery CSS

--- functions.php
+++ functions.php
@@ -1,6 +1,9 @@
 <?php
-// function
+// functions
+//
+// disable CSS on [gallery] inside of each posts 
+// (https://css-tricks.com/snippets/wordpress/remove-gallery-inline-styling/)
 
-
+add_filter( 'use_default_gallery_style', '__return_false' );
 
 ?>
