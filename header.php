abf0ba4efccd39d8c23011146df5f27d34174b83
Kaai Suzuki <suzydp@me.com>
2019-04-11 12:03:55 -0700

    Add bootstrap (instantly)

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Z magazine</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" media="screen" href="<?= get_template_directory_uri(); ?>/assets/css/main.css">
   <?php wp_head(); // apply the admin bar ?>
 </head>
