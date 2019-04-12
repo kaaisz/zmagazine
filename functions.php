7fe8eb7cd763955304c475c85f7af0e8c977853a
Kaai Suzuki <suzydp@me.com>
2019-04-12 00:31:08 -0700

    Apply function to change all the thumbanil size at the same time

--- functions.php
+++ functions.php
@@ -6,4 +6,23 @@
 
 add_filter( 'use_default_gallery_style', '__return_false' );
 
+add_filter( 'shortcode_atts_gallery', 'meks_gallery_atts', 10, 3);
+
+/*
+  size options
+
+  - thumbnail
+  - medium
+  - large
+  - full
+
+  or, if your theme/plugin generate additional custom sizes you can use them as well
+
+*/
+
+function meks_gallery_atts( $output, $pairs, $atts ) {
+  $output['size'] = 'medium';
+  return $output;
+}
+
 ?>
