20ad5015370792129b0719f2eeb6ddd1b1310e42
Kaai Suzuki <suzydp@me.com>
2019-04-12 21:19:12 -0700

    Adjust header / Apply images

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
       $args = array(
         'post_type' => 'post', // post type
         'posts_per_page' => 10, // numbers of posts
-        'paged' => $paged // to show previous posts(It won't be appeared even the link is active)
+        'paged' => $paged // to show previous posts(It won't be appeared even the link is acti)
       );
       
       $wp_query = new WP_Query( $args ); // set the conditions of fetching the posts data
@@ -21,7 +21,7 @@
           $wp_query -> the_post();
     ?>
       <div class="article">
-        <a href="<?php the_permalink(); ?>"><?php the_content(); ?></a>
+        <a href="<?php the_permalink(); ?>"><img src="<?php echo first_img_to_eyecatch(); ?>" alt=""></a>
       </div>
     <?php
         }
