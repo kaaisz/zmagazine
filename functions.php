3444185250a1144213fe2f56761aa961a346ab92
Kaai Suzuki <suzydp@me.com>
2019-04-12 21:04:55 -0700

    Add function for applying 1st image in the eyecatch

--- functions.php
+++ functions.php
@@ -1,8 +1,11 @@
 <?php
 // functions
 //
-// disable CSS on [gallery] inside of each posts 
-// (https://css-tricks.com/snippets/wordpress/remove-gallery-inline-styling/)
+
+/**************************/
+/* disable CSS on [gallery] inside of each posts 
+   (https://css-tricks.com/snippets/wordpress/remove-gallery-inline-styling/)
+*/
 
 add_filter( 'use_default_gallery_style', '__return_false' );
 
@@ -25,4 +28,27 @@ function change_gallery_atts( $output, $pairs, $atts ) {
   return $output;
 }
 
+/**************************/
+
+
+/**************************/
+/* 
+  place the eyecatch from the first img of article
+*/
+function first_img_to_eyecatch() {
+  global $post, $posts;
+  $first_img = '';
+  ob_start();
+  ob_end_clean();
+  $output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post -> post_content, $matches);
+  $first_img = $matches[1][0];
+
+  // define a default image for just in case if any of image would not find out
+  if(empty($first_img)) { 
+    $first_img = "https://dummyimage.com/600x400/000/fff.png&text=Now+Printing";
+  }
+  return $first_img;
+}
+
+/**************************/
 ?>
